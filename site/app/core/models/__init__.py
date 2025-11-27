d9f817702bfe29437d449dd10787869cce9a3f68
Sasha Shkvarla <118696221+sashok21@users.noreply.github.com>
2025-11-27 10:52:06 +0200

    Delete site directory

--- site/app/core/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import BaseModel
-from .category import Category
-from .brand import Brand
-from .product import Product
-from .user import User
-from .order import Order
-from .order_item import OrderItem
